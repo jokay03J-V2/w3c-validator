4ec257e7d66260f587b16acfc2e3cbf1c16a476e
jokay03J-V2 <jmagro@edenschool.fr>
2023-05-22 14:27:32 +0200

    update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,15 +41,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/jokay03J-V2/w3c-validator/tree/docs",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/jokay03J-V2/w3c-validator/tree/docs",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
