5869d8837b0a3b16954fa8df079a98966dfc9b72
jokay03J <magrojoan3@gmail.com>
2023-06-08 22:33:21 +0200

    refactor(types) responseMessage

---
+++ src/utils/displayMessage.ts
@@ -0,0 +1,34 @@
+import { ux } from "@oclif/core";
+import { responseMessage } from "./types";
+import { red, blue, yellow, magenta, grey } from "chalk";
+
+/**
+ * INTERNAL display each response message
+ * @internal
+ * @private
+ */
+export function _displayMessage(messages: responseMessage[]) {
+  for (const message of messages) {
+    switch (message.type) {
+      case "error":
+        ux.log(red(message.type), message.message);
+        break;
+
+      case "info":
+        ux.log(blue(message.type), message.message);
+        break;
+
+      case "warning":
+        ux.log(yellow(message.type), message.message);
+        break;
+
+      case "fatal":
+        ux.log(magenta(message.type), message.message);
+        break;
+
+      default:
+        ux.log(grey(message.type), message.message);
+        break;
+    }
+  }
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,11 @@
+export interface responseMessage {
+  type: string;
+  subType?: string;
+  message: string;
+  lastLine: number;
+  lastColumn: number;
+  firstColumn: number;
+  extract: string;
+  hiliteStart: number;
+  hiliteLenght: number;
+}
