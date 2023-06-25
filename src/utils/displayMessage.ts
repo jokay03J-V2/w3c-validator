4bb644d71f7defe955cc322453b7c966f6db7a6f
jokay03J <magrojoan3@gmail.com>
2023-06-25 15:29:46 +0200

    chore(messages) add last line and extract messages

--- src/utils/displayMessage.ts
+++ src/utils/displayMessage.ts
@@ -1,6 +1,6 @@
 import { ux } from "@oclif/core";
 import { type responseMessage } from "./types";
-import { red, blue, yellow, magenta, grey } from "chalk";
+import { red, blue, yellow, magenta, grey, bgGray, bgBlack } from "chalk";
 
 /**
  * INTERNAL display each response message
@@ -11,23 +11,48 @@ export function _displayMessage(messages: responseMessage[]): void {
   for (const message of messages) {
     switch (message.type) {
       case "error":
-        ux.log(red(message.type), message.message);
+        ux.log(
+          red(message.type),
+          bgGray(`line: ${message.lastLine}`),
+          message.message
+        );
+        ux.log(bgBlack.underline(message.extract));
         break;
 
       case "info":
-        ux.log(blue(message.type), message.message);
+        ux.log(
+          blue(message.type),
+          bgGray(`line: ${message.lastLine}`),
+          message.message
+        );
+        ux.log(bgBlack.underline(message.extract));
         break;
 
       case "warning":
-        ux.log(yellow(message.type), message.message);
+        ux.log(
+          yellow(message.type),
+          bgGray(`line: ${message.lastLine}`),
+          message.message
+        );
+        ux.log(bgBlack.underline(message.extract));
         break;
 
       case "fatal":
-        ux.log(magenta(message.type), message.message);
+        ux.log(
+          magenta(message.type),
+          bgGray(`line: ${message.lastLine}`),
+          message.message
+        );
+        ux.log(bgBlack.underline(message.extract));
         break;
 
       default:
-        ux.log(grey(message.type), message.message);
+        ux.log(
+          grey(message.type),
+          bgGray(`line: ${message.lastLine}`),
+          message.message
+        );
+        ux.log(bgBlack.underline(message.extract));
         break;
     }
   }
