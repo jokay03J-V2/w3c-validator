fbf384231c10f28b1a0840cf75af8f6041eb9121
jokay03J <magrojoan3@gmail.com>
2023-10-22 19:18:06 +0200

    add units tests

---
+++ packages/w3c-validator/test/commands/checkUrl/index.test.ts
@@ -0,0 +1,27 @@
+import {expect, test} from '@oclif/test'
+import {join} from 'path'
+
+const root = join(import.meta.url, '../../../')
+
+describe('check url', () => {
+  test
+    .loadConfig({root})
+    .stdout()
+    .command(['checkUrl', 'https://example.com'])
+    .it('runs check url', (ctx) => {
+      expect(ctx.stdout).to.contain('error')
+      expect(ctx.stdout).to.contain('info')
+      expect(ctx.stdout).to.contain('warning')
+    })
+
+  test
+    .loadConfig({root})
+    .stdout()
+    .command(['checkUrl', 'https://example.com', '-t'])
+    .exit(1)
+    .it('should be exit 1 if t flags is provided ', (ctx) => {
+      expect(ctx.stdout).to.contain('error')
+      expect(ctx.stdout).to.contain('info')
+      expect(ctx.stdout).to.contain('warning')
+    })
+})
