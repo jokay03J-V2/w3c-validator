8d7686556e1343e9ee9b7c2c4bf6a38ff4c34cec
jokay03J <magrojoan3@gmail.com>
2023-10-27 01:29:54 +0200

    create utils functions

---
+++ packages/w3c-validator-vscode/src/utils/showReport.ts
@@ -0,0 +1,98 @@
+import { checkContent } from "w3c-validator-wrapper";
+import * as vscode from "vscode";
+
+enum ActionState {
+  global = "Disable globaly",
+  workspace = "Disable workspace",
+}
+
+export async function showReport(doc: vscode.TextDocument) {
+  const text = doc.getText();
+  vscode.window.withProgress(
+    {
+      cancellable: false,
+      location: vscode.ProgressLocation.Notification,
+    },
+    async (progress) => {
+      progress.report({ message: "Fetching..." });
+      const result = await checkContent(text);
+      if (result.length > 0) {
+        const errors = result.filter((m) => m.type === "error");
+        const infos = result.filter((m) => m.type === "info");
+        const warnings = result.filter((m) => m.type === "warning");
+
+        if (errors.length > 0) {
+          vscode.window
+            .showErrorMessage<ActionState>(
+              `Found ${errors.length} errors`,
+              ActionState.global,
+              ActionState.workspace
+            )
+            .then(handleDisable);
+        } else if (warnings.length > 0) {
+          vscode.window
+            .showWarningMessage<ActionState>(
+              `Found ${warnings.length} warnings`,
+              ActionState.global,
+              ActionState.workspace
+            )
+            .then(handleDisable);
+        } else if (infos.length > 0) {
+          vscode.window
+            .showInformationMessage(
+              `Found ${infos.length} infos`,
+              ActionState.global,
+              ActionState.workspace
+            )
+            .then(handleDisable);
+        } else {
+          vscode.window
+            .showInformationMessage<ActionState>(
+              "Run detailed report command",
+              ActionState.global,
+              ActionState.workspace
+            )
+            .then(handleDisable);
+        }
+      } else {
+        vscode.window
+          .showInformationMessage<ActionState>(
+            "This file is clean",
+            ActionState.global,
+            ActionState.workspace
+          )
+          .then(handleDisable);
+      }
+    }
+  );
+}
+
+function handleDisable(val: ActionState | undefined) {
+  if (val !== undefined) {
+    const config = vscode.workspace.getConfiguration("w3c-validator");
+    switch (val) {
+      case ActionState.global:
+        config.update(
+          "checkFileOnSave",
+          false,
+          vscode.ConfigurationTarget.Global
+        );
+        vscode.window.showInformationMessage(
+          "Disabled fetch on save on globaly"
+        );
+        break;
+
+      case ActionState.workspace:
+        config.update(
+          "checkFileOnSave",
+          false,
+          vscode.ConfigurationTarget.Workspace
+        );
+        vscode.window.showInformationMessage(
+          "Disabled fetch on save on this workspace"
+        );
+        break;
+    }
+    return;
+  }
+}
